fa1eb35978f219565ee2aea78d9cb6f55a46be5f
maxi <mcartes2017@alu.uct.cl>
2024-02-20 12:53:33 -0400

    .

--- resources/views/auth/ingresar.blade.php
+++ resources/views/auth/ingresar.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Inicio de sesión - Vinculamos</title>
+    <title>Inicio de sesión - Vinculamos.</title>
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/reset.password.css') }}">
